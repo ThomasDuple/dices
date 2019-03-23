e70dcaac4ccf5469cceb26a69c89f25cb156ad71
ThomasDuple <ThomasDuple@gmail.com>
2019-03-23 23:54:46 +0100

    dqsdqsdqsd

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 $(document).ready(function() {
-  $('img,br').hide()
+  $('img,br,#bloccount').hide();
+  var count = 0;
+  console.log(count);
 
   $('#btn').on('click', function() {
+    $('#bloccount').show();
+    count = count + 1;
+    console.log('Lancé numero ' + count + ' :')
+    if (count == 1) {
+      $('#count').text('Vous avez fait ' + count + ' seul lancé');
+    } else {
+      $('#count').text('Vous avez fait ' + count + ' lancés');
+    };
+
     var dice1 = (Math.floor(Math.random() * (6 - 1 + 1)) + 1);
     var dice2 = (Math.floor(Math.random() * (6 - 1 + 1)) + 1);
-    var sum = dice1+dice2
-    console.log(dice1)
-    console.log(dice2)
+    var sum = dice1+dice2;
+    console.log('Dé 1 : ' + dice1);
+    console.log('Dé 2 : ' + dice2);
 
-    $('.dice').hide()
-    $('br').show()
+    $('.dice').hide();
+    $('br').show();
 
     if (dice1 == dice2) {
-      $('#score').text('Vous avez fait un score de ' + sum + ' en faisant un double de ' + dice1)
+      $('#score').text('Vous avez fait un score de ' + sum + ' en faisant un double de ' + dice1);
     } else {
-      $('#score').text('Vous avez fait un score de ' + sum + ' en faisant ' + dice1 + ' et ' +  dice2)
+      $('#score').text('Vous avez fait un score de ' + sum + ' en faisant ' + dice1 + ' et ' +  dice2);
     }
 
 
@@ -32,5 +43,5 @@ $(document).ready(function() {
     if (dice2 == 5) { $('#dice2-5').show() };
     if (dice2 == 6) { $('#dice2-6').show() };
 
-  })
+  });
 });
